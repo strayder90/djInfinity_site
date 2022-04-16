17f9606084589fee39555d851c92d10f1a6e3e73
“strayder90” <strayder@live.com>
2022-04-16 13:44:04 +0200

    Updates link to instagram. Adds new phone number and link to soundCloud.

---
+++ dist/main.3129f9ed.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('.carousel').carousel({
+        interval: 5000
+    });
+    $('.carousel').carousel('cycle');
+    $('#top-nav').onePageNav({
+        currentClass: 'active',
+        changeHash: true,
+        scrollSpeed: 500
+    });
+});
+// animation
+$(window).scroll(function() {
+    $('#about,.album,.blogevent ul li').each(function() {
+        var elementPos = $(this).offset().top;
+        var topOfWindow = $(window).scrollTop();
+        if (elementPos < topOfWindow + 400) $(this).addClass("animated fadeInUp");
+    });
+    $('h3,h4').each(function() {
+        var elementPos = $(this).offset().top;
+        var topOfWindow = $(window).scrollTop();
+        if (elementPos < topOfWindow + 500) $(this).addClass("animated fadeInUp");
+    });
+});
+
+//# sourceMappingURL=main.3129f9ed.js.map
